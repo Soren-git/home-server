0c549732f5c2e5dc014bf233a6ba3120a3c02159
Soren-git <soren.anth@gmail.com>
2025-06-24 16:43:30 +0200

    first commit

---
+++ src/utils/useNuiEvent.ts
@@ -0,0 +1,33 @@
+import { MutableRefObject, useEffect, useRef } from 'react';
+import { noop } from './misc';
+
+interface NuiMessageData<T = unknown> {
+  action: string;
+  data: T;
+}
+
+type NuiHandlerSignature<T> = (data: T) => void;
+
+export const useNuiEvent = <T = unknown>(action: string, handler: (data: T) => void) => {
+  const savedHandler: MutableRefObject<NuiHandlerSignature<T>> = useRef(noop);
+
+  // Make sure we handle for a reactive handler
+  useEffect(() => {
+    savedHandler.current = handler;
+  }, [handler]);
+
+  useEffect(() => {
+    const eventListener = (event: MessageEvent<NuiMessageData<T>>) => {
+      const { action: eventAction, data } = event.data;
+
+      if (savedHandler.current) {
+        if (eventAction === action) {
+          savedHandler.current(data);
+        }
+      }
+    };
+
+    window.addEventListener('message', eventListener);
+    return () => window.removeEventListener('message', eventListener);
+  }, [action]);
+};
