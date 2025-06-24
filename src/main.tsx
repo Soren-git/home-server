a7455264e44cb57121ecd42e2420293a8d08fe18
Soren-git <soren.anth@gmail.com>
2025-06-24 16:45:14 +0200

    feat: switch react app to client folder

--- src/main.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-
-// StyleSheets
-import './reset.css';
-
-ReactDOM.createRoot(document.getElementById('root')!).render(<React.StrictMode></React.StrictMode>);
